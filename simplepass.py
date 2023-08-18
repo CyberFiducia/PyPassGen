a36e0a4b8e4200e28c42f00d3a1c5251df3a32f0
Jacques <jacques@jawisoftbcd.com>
2023-08-18 20:38:02 +0200

    added simple password generator

---
+++ simplepass.py
@@ -0,0 +1,27 @@
+import random
+import string
+import argparse
+
+def generate_secure_password(length=12):
+    # Define character sets
+    lowercase_letters = string.ascii_lowercase
+    uppercase_letters = string.ascii_uppercase
+    digits = string.digits
+
+    # Combine character sets
+    all_characters = lowercase_letters + uppercase_letters + digits
+
+    # Generate password
+    secure_password = ''.join(random.choice(all_characters) for _ in range(length))
+    return secure_password
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate a secure password.")
+    parser.add_argument("length", type=int, default=12, nargs='?', help="Length of the password")
+    args = parser.parse_args()
+    
+    secure_password = generate_secure_password(args.length)
+    print("Generated Secure Password:", secure_password)
+
+if __name__ == "__main__":
+    main()
